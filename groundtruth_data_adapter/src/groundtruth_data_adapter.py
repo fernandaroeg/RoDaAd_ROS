b7db6f08f48ce635ee57017a42f944516741dfbc
FernandaRodriguez <fernandaroeg@gmail.com>
2023-04-19 16:53:01 -0500

    add readme, remove old bags, remove / from tfs (noetic fix), fix path drawing

--- groundtruth_data_adapter/src/groundtruth_data_adapter.py
+++ groundtruth_data_adapter/src/groundtruth_data_adapter.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 #Ubuntu 20.04, ROS Noetic, python 3
+#SCRIPT TO CREATE BAG FILE WITH GROUND TRUTH DATA FROM DATASET LOG_ESTIMATED_PATH.TXT FILES 
 import rospy
 import rosbag
 from nav_msgs.msg import Path
@@ -8,11 +9,9 @@ from tf2_msgs.msg import TFMessage
 import tf 
 import numpy as np 
 
-#SCRIPT TO CREATE BAG FILE WITH GROUND TRUTH DATA FROM DATASET LOG_ESTIMATED_PATH.TXT FILES 
-
 #### 0. Parameter Setup ####
-scenario    = rospy.get_param('/groundtruth_data_adapter/environment')
-gtruth_file = rospy.get_param('/groundtruth_data_adapter/gtruth_file')
+scenario      = rospy.get_param('/groundtruth_data_adapter/environment')
+gtruth_file    = rospy.get_param('/groundtruth_data_adapter/gtruth_file')
 file_tstamps= rospy.get_param('/groundtruth_data_adapter/tstamp_file')
 
 
@@ -44,12 +43,12 @@ with open(file_tstamps,'r') as tstamps_file:
         ros_nsecs =  (mrpt_tstamp % 10000000) * 100
         tstamp[item]=rospy.Time(ros_secs,ros_nsecs)#turning the timestamp values to timestamp object
     
- #### 3. Functions to create pose, marker and tf data
-def create_pose_stamped(x, y, theta, t, seq):
+ #### 3. Functions to create pose stamped data
+def create_pose_stamped(x, y, theta, frame, t, seq):
     pose = PoseStamped()
     pose.header.seq = seq
     pose.header.stamp = t
-    pose.header.frame_id = "/g_truth/Pose"
+    pose.header.frame_id = frame
     quaternion_angle = tf.transformations.quaternion_from_euler(0, 0, theta)
     pose.pose = Pose( Point(x, y, 0.0),  Quaternion(*quaternion_angle) )  
     return pose
@@ -76,30 +75,26 @@ def create_TFmsg(x, y, z, theta, frame, child_frame, t, seq):
 #List names with data to write from groundtruth: poseX, poseY, poseTheta, tstamp
 bag = rosbag.Bag('groundtruth'+'_'+scenario+'.bag', 'w')
 file = open('odom_debug.txt', 'w') #create txt file to export odom data for debugging purposes
+path= Path()
 
 for i in range(0,len(poseX)):
-    path= Path()
-    path.header.seq = i
-    path.header.stamp = tstamp[i]
-    path.header.frame_id = "/map"
-    pose_stamped = PoseStamped()
-    quaternion_angle = tf.transformations.quaternion_from_euler(0, 0, poseTheta[i])
-    pose_stamped.pose = Pose( Point(poseX[i], poseY[i], 0.0),  Quaternion(*quaternion_angle) )  
-    pose_stamped.header.seq = i
-    pose_stamped.header.stamp = tstamp[i]
-    pose_stamped.header.frame_id = "/map"
-    path.poses.append(pose_stamped)
 
     #Create pose msg for odometry
-    g_truth_pose = create_pose_stamped(poseX[i], poseY[i], poseTheta[i], tstamp[i], i)
+    g_truth_pose = create_pose_stamped(poseX[i], poseY[i], poseTheta[i], "g_truth/Pose", tstamp[i], i)
+    
+     #Create path msg to visualize g_truth data path in rviz
+    path.header.seq = i
+    path.header.stamp = tstamp[i]
+    path.header.frame_id = "map"
+    path.poses.append(g_truth_pose)
 
-    #Create TF data msg
-    g_truth_path_tf = create_TFmsg(poseX[i], poseY[i], 0, poseTheta[i], "/map", "/Path",tstamp[i], i)
+    #Create TF data for path drawing 
+    g_truth_path_tf = create_TFmsg(poseX[i], poseY[i], 0, poseTheta[i], "map", "Path",tstamp[i], i)
  
     #Write data to bag
-    bag.write("/tf", g_truth_path_tf, tstamp[i])
-    bag.write("/g_truth", path, tstamp[i])
-    bag.write("/g_truth/Pose", g_truth_pose, tstamp[i])
+    bag.write("tf", g_truth_path_tf, tstamp[i])
+    bag.write("Path", path, tstamp[i])
+    bag.write("g_truth/Pose", g_truth_pose, tstamp[i])
     
 bag.close() #export rosbag file to /home/user/.ros 
 file.close() #close debugging txt file

399455ed2488565456ee323fc6b3b8019ef9534b
FernandaRodriguez <fernandaroeg@gmail.com>
2023-06-01 20:09:35 -0600

    get tf params for depth data from launch file

--- rgbd_data_adapter/src/depth_to_laser_adapter.py
+++ rgbd_data_adapter/src/depth_to_laser_adapter.py
@@ -9,18 +9,28 @@ import tf
 from tf.transformations import euler_from_quaternion
 #Node that reads data published from node pointcloud_to_laserscan and rewrites data with correct topic name and framed to be accepted by the AMCL node
 
-pub_scan = rospy.Publisher('/scan', LaserScan, queue_size = 10)
-pub_tf   = rospy.Publisher("/tf", TFMessage, queue_size=10)   
+#Get Launch File Parameters
+TF_x      = rospy.get_param('/depth_to_laser_adapter/TF_x')
+TF_y      = rospy.get_param('/depth_to_laser_adapter/TF_y')
+TF_z      = rospy.get_param('/depth_to_laser_adapter/TF_z')
+TF_yaw    = rospy.get_param('/depth_to_laser_adapter/TF_yaw')
+rgbd_id = rospy.get_param('/depth_to_laser_adapter/rgbd_id')
+pc_to_laser_topic = rospy.get_param('/depth_to_laser_adapter/pc_to_laser_topic')
+
+pub_scan = rospy.Publisher('depth'+str(rgbd_id), LaserScan, queue_size = 11)
+pub_tf   = rospy.Publisher("tf", TFMessage, queue_size=11)   
 scan_received = LaserScan()    
 
-def create_TFmsg(x, y, z, theta, frame, child_frame):
+def create_TFmsg(x, y, z, yaw, frame, child_frame, seq, t):
     trans = TransformStamped()
+    trans.header.seq = seq
+    trans.header.stamp = t
     trans.header.frame_id = frame
     trans.child_frame_id = child_frame
     trans.transform.translation.x = x
     trans.transform.translation.y = y
     trans.transform.translation.z = z
-    q = tf.transformations.quaternion_from_euler(0,0,theta)
+    q = tf.transformations.quaternion_from_euler(0,0,yaw)
     trans.transform.rotation.x = q[0]
     trans.transform.rotation.y = q[1]
     trans.transform.rotation.z = q[2]
@@ -30,26 +40,26 @@ def create_TFmsg(x, y, z, theta, frame, child_frame):
     return msg
 
 def callback(msg):
-    current_time = rospy.Time.now()
-    scan_received.header.seq      = msg.header.seq 
-    scan_received.header.stamp    = current_time
-    scan_received.header.frame_id = '/laser'
-    scan_received.angle_min       = msg.angle_min      
-    scan_received.angle_max       = msg.angle_max      
-    scan_received.angle_increment = msg.angle_increment
-    scan_received.time_increment  = msg.time_increment 
-    scan_received.scan_time       = msg.scan_time      
-    scan_received.range_min       = msg.range_min      
-    scan_received.range_max       = msg.range_max      
-    scan_received.ranges          = msg.ranges         
+    current_time = rospy.Time(0)
+    scan_received.header.seq         = msg.header.seq 
+    scan_received.header.stamp     = current_time
+    scan_received.header.frame_id = 'depth'+str(rgbd_id)
+    scan_received.angle_min           = msg.angle_min      
+    scan_received.angle_max          = msg.angle_max      
+    scan_received.angle_increment= msg.angle_increment
+    scan_received.time_increment   = msg.time_increment 
+    scan_received.scan_time            = msg.scan_time      
+    scan_received.range_min           = msg.range_min      
+    scan_received.range_max          = msg.range_max      
+    scan_received.ranges                  = msg.ranges         
     
+    new_tf_msg   = create_TFmsg(TF_x, TF_y, TF_z, TF_yaw, 'base_link','depth'+str(rgbd_id), msg.header.seq+1, current_time)
     pub_scan.publish(scan_received)
-    new_tf_msg   = create_TFmsg(0.285,0,1.0450,0,'/base_link','/laser')
     pub_tf.publish(new_tf_msg)
 
 def listener():
     rospy.init_node('depth_to_laser_adapter', anonymous=True)
-    sub = rospy.Subscriber('/camera/RGB1/scan', LaserScan, callback)
+    sub = rospy.Subscriber(pc_to_laser_topic, LaserScan, callback)
     rospy.spin()
 
 if __name__ == '__main__':
